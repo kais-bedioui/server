a0a10a78f9c3b4a7a69e875b7c72610658934f10
GuanLuo <41310872+GuanLuo@users.noreply.github.com>
2021-11-24 10:48:29 -0800

    Revert refactor change

--- qa/L0_long_running_stress/scenarios.py
+++ qa/L0_long_running_stress/scenarios.py
@@ -263,7 +263,7 @@ class PerfAnalyzerScenario(Scenario):
                 if dtype == np.float32:
                     res = float(res)
                 elif dtype == np.int32:
-                    # Do nothing
+                    res = int(res)
                 elif dtype == np.dtype(object):
                     res = str(res)
                 output_data.append(
